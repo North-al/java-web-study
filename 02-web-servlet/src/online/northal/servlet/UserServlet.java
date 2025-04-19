67116f548ab3cc5770ac682518c4af36763adc3b
North <1227379879@qq.com>
2025-04-19 14:45:16 +0800

    feat: 新增hello world示例

---
+++ 02-web-servlet/src/online/northal/servlet/UserServlet.java
@@ -0,0 +1,18 @@
+package online.northal.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        System.out.println(username);
+
+        resp.getWriter().write("<h1>" + username + "</h1>");
+    }
+}
